5adb4fb0995f5671afd79d6e724ba93e5865dbab
Priyanka Sarkar <Priyanka_Sarkar@epam.com>
2019-04-05 13:03:29 +0530

    Implementation of Disjoint-Set Data Structure using Union-Find Algorithm

---
+++ src/com/implementation/disjointset/UnionFind.java
@@ -0,0 +1,59 @@
+package com.implementation.disjointset;
+
+/**
+ * 
+ * @author Priyanka_Sarkar
+ *
+ */
+
+public class UnionFind {
+	
+	private int size;
+	private int numSets;
+	private int[] id;
+	private int[] rank;
+	
+	public UnionFind(int size) {
+		this.size = size;
+		this.numSets = size;
+		this.id = new int[size];
+		this.rank = new int[size];
+		createSet();
+	}
+	
+	private void createSet() {
+		for(int i = 0; i < size; i++) {
+			id[i] = i;
+		}
+	}
+	
+	public void union(int element1, int element2) {
+		int root1 = find(element1);
+		int root2 = find(element2);
+		if(root1 == root2) {
+			return;
+		}
+		if(rank[root1] < rank[root2]) {
+			id[root1] = root2;
+		}
+		else if(rank[root2] < rank[root1]) {
+			id[root2] = root1;
+		}
+		else {
+			id[root2] = root1;
+			rank[root1]++;
+		}
+	}
+	
+	public int find(int element) {
+		return id[element];
+	}
+	
+	public boolean isConnected(int element1, int element2) {
+		return find(element1) == find(element2);
+	}
+	
+	public int getNumSets() {
+		return numSets;
+	}
+}
